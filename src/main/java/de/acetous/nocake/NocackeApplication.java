454054477187a1b6bff1317801932eabce9da0f2
Sebastian Herbermann <sebastian.herbermann@adesso.de>
2016-06-24 13:13:01 +0200

    inital import

---
+++ src/main/java/de/acetous/nocake/NocackeApplication.java
@@ -0,0 +1,12 @@
+package de.acetous.nocake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NocackeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NocackeApplication.class, args);
+	}
+}
---
+++ src/test/java/de/acetous/nocake/NocackeApplicationTests.java
@@ -0,0 +1,18 @@
+package de.acetous.nocake;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = NocackeApplication.class)
+@WebAppConfiguration
+public class NocackeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
